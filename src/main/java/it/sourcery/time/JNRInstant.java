e3b18b4cf216447dcfd9a99de8c1f85c8f31b28d
Michael André Pearce <michael.andre.pearce@me.com>
2017-01-13 07:18:12 +0000

    Update JNRInstant.java

--- src/main/java/it/sourcery/time/JNRInstant.java
+++ src/main/java/it/sourcery/time/JNRInstant.java
@@ -22,7 +22,7 @@ public class JNRInstant {
         }
     }
 
-    public interface LibC  {
+    public interface LibC {
         public int gettimeofday(@Out @Transient Timeval tv, Pointer unused);
 
     }
@@ -31,33 +31,10 @@ public class JNRInstant {
     static Runtime runtime = Runtime.getRuntime(libc);
     static Timeval time = new Timeval(runtime);
 
-    public static Instant now(){
+    public static Instant now() {
         libc.gettimeofday(time, null);
         long seconds= time.tv_sec.get();
         long micros = time.tv_usec.get() & 4294967295L;
         return Instant.ofEpochSecond(seconds, micros * 1000);
     }
-
-    public static void main(String... args) throws InterruptedException {
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        System.out.println(JNRInstant.now());
-        Instant instant = JNRInstant.now();
-
-        Thread.sleep(1000);
-        Instant instant2 = JNRInstant.now();
-
-        Duration duration = Duration.between(instant, instant2);
-        long micro = TimeUnit.SECONDS.toMicros(duration.getSeconds()) + TimeUnit.NANOSECONDS.toMicros(duration.getNano());
-        System.out.println(micro);
-    }
-
-
-
-
 }
